e44a11e2ed63b41dda3c4f7a39838e420c94c5d0
Дмитрий <dimap_2001@inbox.ru>
2023-11-14 13:48:30 +0300

    add

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,14 +1,14 @@
 import random
 
 
-GAME_TASK = "Answer 'yes' if the number is even, otherwise answer 'no'."
+GAME_TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def is_even(question):
     if question % 2 == 0:
-        return "yes"
+        return 'yes'
     else:
-        return "no"
+        return 'no'
 
 
 def game_task():
